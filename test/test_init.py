3486792c6b756d876c7b6ad0efa0cb9032c5db2b
jepidoptera <jepidoptera@live.com>
2024-05-15 11:38:39 +0200

    add test for total issuance query and fix test_hydradx_init (asset 2 doesn't exist)

--- test/test_init.py
+++ test/test_init.py
@@ -2,16 +2,28 @@ from hydradxapi import HydraDX
 
 HYDRA_MAINNET = "wss://hydradx-rpc.dwellir.com"
 
+
 def test_hydradx_init():
     hydra = HydraDX(HYDRA_MAINNET)
+
+    # HDX
     hydra.connect()
     s = hydra.api.omnipool.asset_state(0)
     print(s)
     hydra.close()
 
+    # GLMR
     hydra.connect()
-
-    s = hydra.api.omnipool.asset_state(2)
+    s = hydra.api.omnipool.asset_state(16)
     print(s)
     hydra.close()
 
+
+def test_total_issuance():
+    hydra = HydraDX(HYDRA_MAINNET)
+    hydra.connect()
+    total = hydra.api.tokens.total_issuance(101)
+    print(total)
+    if not total > 0:
+        raise AssertionError("No issuance found for BTC 2-Pool.")
+    hydra.close()
